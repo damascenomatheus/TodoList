0b4847fff7fa9b9862d66d2766d8cdb1c48cc7d8
Matheus Damasceno <matheusdamascenotx@gmail.com>
2025-12-25 18:35:54 -0300

    initial app configuration

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Matheus Damasceno on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
