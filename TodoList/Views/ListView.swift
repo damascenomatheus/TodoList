ccdf5775cf872612e22946b3b485623d23c94b73
Matheus Damasceno <matheusdamascenotx@gmail.com>
2025-12-25 18:44:22 -0300

    Add list view to navigation stack

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        }
+        .navigationTitle("Title")
     }
 }
 
 #Preview {
-    ListView()
+    NavigationStack {
+        ListView()
+    }
 }
